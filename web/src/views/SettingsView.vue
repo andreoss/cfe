4ec35770eae0527230825bb550d0cdabfc1c1af5
andreoss <andreoss@sdf.org>
2025-12-07 14:41:02 -0300

    Stop announcing a note that never changes

--- web/src/views/SettingsView.vue
+++ web/src/views/SettingsView.vue
@@ -201,7 +201,7 @@ async function onEndAllSessions() {
           <option value="compact">Compact</option>
         </select>
       </label>
-      <p role="status">These are kept in this browser.</p>
+      <p class="note">These are kept in this browser.</p>
     </section>
     <p v-if="!auth.currentUser">Sign in to manage your account.</p>
     <template v-else>
